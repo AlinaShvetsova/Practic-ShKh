72a8562bb378afce4bf053cccfc4aa2ce342def5
Veronika2125 <62997381+Veronika2125@users.noreply.github.com>
2020-11-09 20:34:46 +0300

    Update zadanie5.py

--- pact 2/zadanie5.py
+++ pact 2/zadanie5.py
@@ -2,20 +2,16 @@ import numpy as np
 import math
 
 
-def func(n):
-    print("Введите размер матрицы")
-    n = int(input())
-    a = np.random.randint(0,10,(n))
-    b = np.random.randint(0,10,(n))
-    
-    print(a, '\n', b)
-    
+def EuclideanDistance():
     diff = [(a[i] - b[i])**2 for i in range(len(a))]
-    print(diff)
-    
     sumEl = sum(diff)
-    print(sumEl)
-    
     evRast = math.sqrt(sumEl)
-    print(evRast)
-func('n')
+    return evRast
+    
+print("Введите размер матрицы")
+n = int(input())
+a = np.random.randint(0,10,(n))
+b = np.random.randint(0,10,(n))
+print(a, '\n', b)
+print(EuclideanDistance())
+print('')
