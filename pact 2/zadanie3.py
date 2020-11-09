d5c3bcbcd1759479a5b1e3eccfe53bb00f593866
Veronika2125 <62997381+Veronika2125@users.noreply.github.com>
2020-11-09 20:59:32 +0300

    Update zadanie3.py

--- pact 2/zadanie3.py
+++ pact 2/zadanie3.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-def Task3(a):
-    b = np.zeros(len(a))
-    b[:len(a[x:])] = a[x:]
-    b[len(a[x:]):] = a[:x]
+def СyclicShift(a):
+    b = a[x:]+a[:x]
     return b
 
 a = [1, 2, 3, 4, 5]  
 print (a) # вывод Массива
 print('Введите  число, на которое сместить массив: \t')
 x = int(input())
-print(Task3(a))
+print(СyclicShift(a))
+print('')
