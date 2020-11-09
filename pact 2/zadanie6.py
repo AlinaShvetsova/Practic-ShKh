74987fdb7cdc9d88d6e96f467f886375b7a6b48d
Veronika2125 <62997381+Veronika2125@users.noreply.github.com>
2020-11-09 20:56:08 +0300

    Update zadanie6.py

--- pact 2/zadanie6.py
+++ pact 2/zadanie6.py
@@ -1,19 +1,22 @@
 import numpy as np;
 
 
-def func(n):
-    
-    print('Введите размер квадратной матрицы')
-    n=int(input())
-    if n % 2 == 0:
-        a = np.random.randint(0, 10, (n, n))
+def SumOfElements():
+    if n == m:
+        a = np.random.randint(0, 10, (n, m))
+        k = 0
         z=[a[i][i] for i in range(n)]
-        print('Полученная матрица')
-        print(a)
-        print('Массив элементов на главной диагонали')
-        print(z)
-        print('Результат')
-        print(sum(z))
-    else:
-        print('Вы ввели размер не квадратной матрицы!')
-func('n')
+        for i in range(len(z)): 
+            k = k + z[i]
+        return k
+    else: return -1
+    
+print('Введите размер квадратной матрицы')
+n=int(input())
+m=int(input())
+k = SumOfElements()
+if(k == -1): 
+    print('Вы ввели размер не квадратной матрицы!')
+else: 
+    print(k)
+print('')
