0e1e5983153f65a86be6d94fe817b247a445b77d
Veronika2125 <62997381+Veronika2125@users.noreply.github.com>
2020-11-16 13:18:22 +0300

    Update k_means.py
    Расстояние городских кварталов - это расстояние между двумя точками.
    В результате получаем сумму модулей разностей координат между двумя точками.

--- pract-04/k_means.py
+++ pract-04/k_means.py
@@ -1,10 +1,22 @@
-﻿import math
+import math
 import numpy as np
 
 # евклидово расстояние между двумя точками
 def dist(A, B):
   r=math.sqrt(sum((A-B)**2))
   return r
+
+# расстояние городских кварталов
+def CityQuarters(L,P): 
+  # расстояние городских кварталов между точками L и P
+  result = math.fabs(L[0] - P[0]) + math.fabs(L[1] - P[1])
+  #  в результате получаем сумму модулей разностей координат между двумя точками
+  return result
+
+# Пример расстояния городских кварталов
+L = [70, 40]
+P = [330, 220]
+print(CityQuarters(L, P))
 
 # возвращает список индексов ближайших центров по каждой точке
 def class_of_each_point(X, centers):
@@ -28,7 +40,7 @@ def kmeans(k, X):
   # приписываем каждую точку к заданному классу
   curr_iteration = class_of_each_point(X, centers)
   
-  while True:
+  while np.any(curr_iteration != prev_iteration):
     prev_iteration = curr_iteration
     # вычисляем новые центры масс
     for i in range(k):
@@ -37,6 +49,4 @@ def kmeans(k, X):
         centers[i,:] = np.mean(sub_X, axis=0)
     # приписываем каждую точку к заданному классу
     curr_iteration = class_of_each_point(X, centers)
-    if np.all(curr_iteration == prev_iteration):
-      break
   return centers
