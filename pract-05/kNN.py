5840557909a7e7e4dd2adc62e664b3fa954074b6
AlinaShvetsova <61935822+AlinaShvetsova@users.noreply.github.com>
2020-11-22 21:43:39 +0300

    Update kNN.py

--- pract-05/kNN.py
+++ pract-05/kNN.py
@@ -1,27 +1,20 @@
 import numpy as np
 
-def k_nearest(X, y, k, obj, CityQuarters):
-   
+def k_nearest(X, y, k, obj, dist_function):
     # нормализация каждого столбца (кроме последнего)
     mx=np.max(X, axis=0)
     norm_X = X/mx
     
     # нормализация объекта obj 
-    m = X.shape[0]
     norm_obj = obj/mx
     
-    distance = np.zeros((m, X.shape[1]))
-    for i in range(m):
-        distance[i] = CityQuarters(norm_X[i], norm_obj)
+    distance = [dist_function(norm_item, norm_obj) for norm_item in norm_X]
    
     # получение с помощью функции np.argsort индексы соседей по мере их удаления от obj.
     sort = np.argsort(distance, axis=0)
     
     # выборка в отдельный вектор классы k ближайших соседей
-    nearest_classes = np.zeros((k))
-    for i in range(k):
-        index = sort[i,0]
-        nearest_classes[i] = y[index] 
+    nearest_classes = y[sort[:k]]
 
     # наиболее часто встречающийся класс в этом векторе
     unique, counts = np.unique(nearest_classes, return_counts=True)
