3f35a0a2afdacc40f816107f1e1499ed3c09b1e2
AlinaShvetsova <61935822+AlinaShvetsova@users.noreply.github.com>
2020-11-22 14:14:03 +0300

    Update run.py

--- pract-05/run.py
+++ pract-05/run.py
@@ -1,5 +1,18 @@
 import numpy as np
 from kNN import k_nearest
+import math
+
+# вычисление евклидова расстояния между двумя точками
+def EuclideanDistance(p1, p2):
+    result1 = math.sqrt(sum((p1-p2)**2))
+    return result1
+
+def CityQuarters(a, b):
+    # расстояние городских кварталов
+    result = math.fabs(a[0] - b[0]) + math.fabs(a[1] - b[1])
+    # в результате получаем сумму модулей разностей координат между двумя точками
+    return result
+
 
 X = np.array([[33, 21, 1],
               [41, 13, 1],
@@ -18,14 +31,13 @@ X = np.array([[33, 21, 1],
               [135, 47, 4],
               [138, 66, 4]])
 
+
 height = int(input("Введите рост: "))
 weight = int(input("Введите вес: "))
 obj = np.array([height, weight])
-
-# классификация методом k ближайших соседей
 k = 3
-object_class = k_nearest(X, k, obj)
-
+object_class = k_nearest(X[:, 0:-1], X[:,2], k, obj, EuclideanDistance)
 # вывод результата классификации
 monkeys = {1: 'lemur', 2: 'schimpanze', 3: 'gorilla', 4: 'orangutan'}
 print(monkeys[object_class])
+
