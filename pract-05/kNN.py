d04493f87ac8d04fffe9a33aea48e244a5aca263
AlinaShvetsova <61935822+AlinaShvetsova@users.noreply.github.com>
2020-11-22 14:14:24 +0300

    Update kNN.py

--- pract-05/kNN.py
+++ pract-05/kNN.py
@@ -1,33 +1,29 @@
 import numpy as np
-import math
 
-def k_nearest(X, k, obj):
-    
+def k_nearest(X, y, k, obj, CityQuarters):
+   
     # нормализация каждого столбца (кроме последнего)
-    sub_X = X[:, 0:-1]
-    mx=np.max(sub_X, axis=0)
-    norm_X = sub_X/mx
+    mx=np.max(X, axis=0)
+    norm_X = X/mx
     
-    # нормализация объекта obj
+    # нормализация объекта obj 
+    m = X.shape[0]
     norm_obj = obj/mx
-    distance = np.zeros((16, k-1))
-    for i in range(16):
-       distance[i] = dist(norm_X[i], norm_obj)
+    
+    distance = np.zeros((m, X.shape[1]))
+    for i in range(m):
+        distance[i] = CityQuarters(norm_X[i], norm_obj)
    
     # получение с помощью функции np.argsort индексы соседей по мере их удаления от obj.
     sort = np.argsort(distance, axis=0)
     
     # выборка в отдельный вектор классы k ближайших соседей
-    nearest_classes = np.zeros((4))
-    for i in range(4):
+    nearest_classes = np.zeros((k))
+    for i in range(k):
         index = sort[i,0]
-        nearest_classes[i] = X[[index], -1]   
+        nearest_classes[i] = y[index] 
 
     # наиболее часто встречающийся класс в этом векторе
     unique, counts = np.unique(nearest_classes, return_counts=True)
     object_class = unique[np.argmax(counts)]
     return object_class
-
-# вычисление евклидова расстояния между двумя точками
-def dist(p1, p2):
-    return math.sqrt(sum((p1-p2)**2))
